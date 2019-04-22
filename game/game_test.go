ee181a5b322cba5376b58ada664a15b1bea3e8b1
Riley Mulford <rdmulfor@mtu.edu>
2019-04-22 12:29:18 -0400

    add tests and fix travis support

---
+++ game/game_test.go
@@ -0,0 +1,37 @@
+package game
+
+import (
+	"testing"
+)
+
+func TestLoadLevelFromFile(t *testing.T) {
+
+}
+
+func TestCanWalk(t *testing.T) {
+
+}
+
+func TestCheckDoor(t *testing.T) {
+
+}
+
+func TestHandleInput(t *testing.T) {
+
+}
+
+func TestGetNeighbors(t *testing.T) {
+
+}
+
+func TestBfs(t *testing.T) {
+
+}
+
+func TestAstar(t *testing.T) {
+
+}
+
+func TestRun(t *testing.T) {
+
+}
---
+++ ui/ui_test.go
@@ -0,0 +1,25 @@
+package ui
+
+import (
+	"testing"
+)
+
+func TestLoadTextureIndex(t *testing.T) {
+
+}
+
+func TestImgFileToTexture(t *testing.T) {
+
+}
+
+func TestPixelsToTexture(t *testing.T) {
+
+}
+
+func TestDraw(t *testing.T) {
+
+}
+
+func TestGetInput(t *testing.T) {
+
+}
