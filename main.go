aec5c12f247dc95967d5682738f10bf33147a399
Riley Mulford <rdmulfor@mtu.edu>
2019-04-26 12:09:16 -0400

    fix random tile placement

--- main.go
+++ main.go
@@ -2,13 +2,18 @@
 package main
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/rdmulford/rirpg/game"
 	"github.com/rdmulford/rirpg/ui2d"
 	"github.com/rdmulford/rirpg/worldgen"
 )
 
 func main() {
-	worldgen.GenerateNewLevel(50, 50, 10)
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+	worldgen.GenerateNewLevel(50, 50, int64(r.Intn(1000)))
 	game := game.NewGame(1, "game/maps/level1.map")
 	go func() { game.Run() }()
 	ui := ui2d.NewUI(game.InputChan, game.LevelChans[0])
--- worldgen/worldgen.go
+++ worldgen/worldgen.go
@@ -5,67 +5,62 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
-)
 
-type pos struct {
-	x, y int
-}
+	"github.com/rdmulford/rirpg/game"
+)
 
 // utilize perlin noise to generate new level file at game/maps/level1.map
 func GenerateNewLevel(xSize, ySize int, seed int64) {
-	genMap := make([][]rune, ySize)
+	genMap := make([][]game.Tile, ySize)
 	for i := range genMap {
-		genMap[i] = make([]rune, xSize)
+		genMap[i] = make([]game.Tile, xSize)
 	}
-	dirtTiles := make([]pos, 0)
+	openTiles := make([]game.Pos, 0)
 
+	// define world tiles base don perlin noise
 	p := NewPerlin(2, 2, 3, seed)
 	for y := 0; y < ySize; y++ {
 		for x := 0; x < xSize; x++ {
 			val := p.Noise2D(float64(x)/10, float64(y)/10)
 			if x == 0 || x == xSize-1 || y == 0 || y == ySize-1 {
-				genMap[x][y] = '#'
+				genMap[x][y] = game.Tile('#')
 				continue
 			}
 			if val < -0.3 {
-				genMap[x][y] = '~'
+				genMap[x][y] = game.Tile('~')
 			} else if val >= -0.3 && val < 0.3 {
-				genMap[x][y] = ','
+				genMap[x][y] = game.Tile(',')
+				openTiles = append(openTiles, game.Pos{x, y})
 			} else if val >= 0.3 {
-				genMap[x][y] = '.'
-				dirtTiles = append(dirtTiles, pos{x, y})
+				genMap[x][y] = game.Tile('.')
+				openTiles = append(openTiles, game.Pos{x, y})
 			}
 		}
-	}
-
-	// place monsters, overwriting old tiles (that could be bad?)
-	r := rand.New(rand.NewSource(1))
-	r.Seed(seed)
-	for i := 0; i < 1; i++ {
-		genMap[r.Intn(xSize-2)][r.Intn(ySize-2)] = 'R'
-	}
-	for i := 0; i < 1; i++ {
-		genMap[r.Intn(xSize-2)][r.Intn(ySize-2)] = 'S'
 	}
 
 	// place trees
 	for i := 0; i < 200; i++ {
-		rX := r.Intn(xSize - 2)
-		rY := r.Intn(ySize - 2)
-		if genMap[rX][rY] == ',' {
-			genMap[rX][rY] = '^'
-		}
+		placeTile(openTiles, genMap, game.Tile('^'))
+	}
+
+	// place monsters
+	for i := 0; i < 2; i++ {
+		placeTile(openTiles, genMap, game.Tile('R'))
+	}
+	for i := 0; i < 2; i++ {
+		placeTile(openTiles, genMap, game.Tile('S'))
 	}
 
 	// place player
-	playerPos := dirtTiles[rand.Intn(len(dirtTiles))]
-	genMap[playerPos.x][playerPos.y] = '@'
+	placeTile(openTiles, genMap, game.Tile('@'))
 
+	// delete old level
 	err := os.Remove("game/maps/level1.map")
 	if err != nil {
 		panic(err)
 	}
 
+	// write genMap to new level file
 	f, err := os.Create("game/maps/level1.map")
 	if err != nil {
 		panic(err)
@@ -85,4 +80,17 @@ func GenerateNewLevel(xSize, ySize int, seed int64) {
 		}
 	}
 	w.Flush()
+}
+
+// place a new tile on an open tile
+func placeTile(openTiles []game.Pos, genMap [][]game.Tile, tile game.Tile) {
+	index := rand.Intn(len(openTiles))
+	mPos := openTiles[index]
+	remove(openTiles, index)
+	genMap[mPos.X][mPos.Y] = tile
+}
+
+func remove(s []game.Pos, i int) []game.Pos {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
 }
